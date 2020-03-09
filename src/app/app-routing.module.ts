df276fe979423d5768d1998afa57eba33e76f15a
Falix <30694778+falixchong@users.noreply.github.com>
2020-03-09 15:37:12 +0800

    Create basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { JoinGameComponent } from './components/join-game/join-game.component';
+import { HostGameComponent } from './components/host-game/host-game.component';
+import { PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterUserComponent},
+  { path: 'sidenav', component: SidenavComponent},
+  { path: 'join-game', component: JoinGameComponent},
+  { path: 'host-game', component: HostGameComponent},
+  { path: 'login', component: LoginComponent, pathMatch: 'full'},
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes, 
+    { enableTracing: true } // <-- debugging purposes only
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { AngularMaterialModule } from './material-module';
+import { HostGameComponent } from './components/host-game/host-game.component';
+import { JoinGameComponent } from './components/join-game/join-game.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterUserComponent,
+    SidenavComponent,
+    HostGameComponent,
+    JoinGameComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
