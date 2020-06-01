20aff115799835ba490802c07c4aafe3ec4318b0
Falix <30694778+falixchong@users.noreply.github.com>
2020-06-01 11:46:40 +0800

    update registration page

--- src/app/register/register/register.component.ts
+++ src/app/register/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'app-register',
@@ -11,11 +11,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 	serviceErrors: any = {};
 	registerForm: FormGroup;
+	isRegistered: Boolean = false;
 
 	constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
 		this.registerForm = this.fb.group({
 			userName: [ null, [ Validators.required, Validators.maxLength(20) ] ],
-			email: [ null, [ Validators.required, Validators.maxLength(60) ] ],
+			email: new FormControl(
+				null,
+				Validators.compose([
+					Validators.required,
+					Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+				])
+			),
 			password: [ null, [ Validators.required, Validators.maxLength(60) ] ]
 		});
 	}
@@ -26,11 +33,14 @@ export class RegisterComponent implements OnInit {
 		console.log(this.registerForm.value);
 		this.http.post('/api/v1/register', this.registerForm.value).subscribe(
 			(data: any) => {
-				let path = 'main/sport-game/' + data.gid;
+				console.log(data);
+				this.isRegistered = true;
+				let path = 'login';
 				this.router.navigate([ path ]);
 			},
 			(error) => {
 				this.serviceErrors = error.error.error;
+				this.isRegistered = false;
 			}
 		);
 	}
